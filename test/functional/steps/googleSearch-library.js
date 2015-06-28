6999e0a2c14d4af189158214205f39cb02307299
Gonzalo Zunino <gonzalo.zunino@gmail.com>
2015-06-28 11:35:19 -0300

    Update googleSearch-library.js

--- test/functional/steps/googleSearch-library.js
+++ test/functional/steps/googleSearch-library.js
@@ -5,11 +5,11 @@ var library = require("yadda").localisation.English.library();
 
 //Scenario 1
 library.when("I search for for money", function() {
-    this.controller.checkElement('.header-logo.lan-tam');
+    this.controller.checkElement('.');
 });
 
 library.then("About 2,640,000,000 results gets displayed", function() {
-    this.controller.checkElement('.header-logo.lan-tam');
+    this.controller.checkElement('.');
 });
 
 module.exports = library;
